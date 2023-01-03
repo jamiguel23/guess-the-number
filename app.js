fdd6f5e956355152ac81c24a5b4abe297f3bb211
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-03 11:18:23 -0800

    working on project

--- app.js
+++ app.js
@@ -2,20 +2,29 @@
 
 // console.log('hello world')
 
-let num = Math.floor(Math.random() * 11);
+// let num = Math.floor(Math.random() * 11);
+
+let num = Math.floor(Math.random() * 10) +1;
 
 
 
-function guessNum(){
-  
+
+function guessNum() {
+
   let tries = 3
   console.log(num)
-  
-  for(let i = 0; i < tries; i++){
+
+  for (let i = 0; i < tries; i++) {
     let guess = prompt("guess a number between 1 and 10");
+    let attempts = 1
 
     if (num == guess) {
-      alert("Got it!");
+      attempts++
+      console.log(attempts)
+      alert("Got it! It took you " + attempts + " attempts");
+      break
+    } else if( guess> num) {
+
     } else {
       alert("Sorry!  You guessed " + guess + " and the number was " + num);
     }
