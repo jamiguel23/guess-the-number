f0378d9de4bf8a10c55d1a947acb925315a0c98e
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-03 11:36:21 -0800

    finished assignment, i think

--- app.js
+++ app.js
@@ -12,28 +12,55 @@ let num = Math.floor(Math.random() * 10) +1;
 function guessNum() {
 
   let tries = 3
-  console.log(num)
+  let count = 3
+  // console.log(num)
+  let attempts = 0
 
   for (let i = 0; i < tries; i++) {
-    let guess = prompt("guess a number between 1 and 10");
-    let attempts = 1
+    let guess = prompt("guess a number between 1 and 10, you have 3 tries");
 
     if (num == guess) {
       attempts++
-      console.log(attempts)
+      // console.log(attempts)
       alert("Got it! It took you " + attempts + " attempts");
       break
 
     } else if(guess> num) {
-      alert('guess is to HIGH. try again')
+
+      if(count == 2){
+        attempts++
+        count--
+        // console.log(attempts)
+        alert('guess is too HIGH. you have ' + count + ' try remaining')
+      } else{
+
+        attempts++
+        count--
+        // console.log(attempts)
+        alert('guess is too HIGH. you have ' + count + ' tries remaining')
+      }
 
     }else if(guess< num) {
-      alert('guess is to LOW. try again')
+      if(count == 2){
+
+        attempts++
+        count--
+        // console.log(attempts)
+        alert('guess is too LOW. you have ' + count + ' try remaining')
+
+      } else{
+
+        attempts++
+        count--
+        // console.log(attempts)
+        alert('guess is too HIGH. you have ' + count + ' tries remaining')
+
+      }
 
     } 
     
-    else {
-      alert("Sorry!  You guessed " + guess + " and the number was " + num);
+    if(attempts === tries) {
+      alert("Sorry! Ran out of tries.  You guessed " + guess + " and the number was " + num);
     }
   }
 }
