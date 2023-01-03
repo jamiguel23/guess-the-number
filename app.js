d59246e2957298bf94c2b81fb4f7802cab801cba
Matt Miguel <75647212+jamiguel23@users.noreply.github.com>
2023-01-03 11:07:51 -0800

    added function

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 'use strict'
 
-console.log('hello world')
+// console.log('hello world')
 
 let num = Math.floor(Math.random() * 11);
 
-let guess = prompt("guess a number between 1 and 10");
 
-if (num == guess) {
-  alert("Got it!");
-} else {
-  alert("Sorry!  You guessed " + guess + " and the number was " + num);
+
+function guessNum(){
+  
+  let tries = 3
+  console.log(num)
+  
+  for(let i = 0; i < tries; i++){
+    let guess = prompt("guess a number between 1 and 10");
+
+    if (num == guess) {
+      alert("Got it!");
+    } else {
+      alert("Sorry!  You guessed " + guess + " and the number was " + num);
+    }
+  }
 }
+
+guessNum()
